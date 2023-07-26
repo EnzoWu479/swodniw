015b2d40cd84ff75ee7cdfc931964db2b833b1b1
EnzoWu479 <enwu2014@hotmail.com>
2023-07-25 21:55:42 -0300

    feat: technologies window

--- src/components/Technologies/Technologies.tsx
+++ src/components/Technologies/Technologies.tsx
@@ -1,11 +1,17 @@
-import { Container } from "./_technologies";
+import { FaReact } from 'react-icons/fa';
+import { Container } from './_technologies';
+import { SiDjango, SiNextdotjs, SiPython, SiTypescript } from 'react-icons/si';
 
 const Technologies = () => {
   return (
     <Container>
-      
+      <SiNextdotjs title="Next.JS" />
+      <FaReact title="React.JS" />
+      <SiTypescript title="TypeScript" />
+      <SiDjango title="Django" />
+      <SiPython title="Python" />
     </Container>
-  )
-}
+  );
+};
 
 export default Technologies;
--- src/components/Technologies/_technologies.ts
+++ src/components/Technologies/_technologies.ts
@@ -2,5 +2,13 @@ import { styled } from 'styled-components';
 import WindowContainer from '../WindowContainer/WindowContainer';
 
 export const Container = styled(WindowContainer).attrs({'data-com': 'Container'})`
-  
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 1rem;
+  flex-wrap: wrap;
+
+  color: #fff;
+  font-size: 3rem;
+
 `;
